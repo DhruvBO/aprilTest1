30aaadbd482ece7ed7be41a4e31104c0dd629d85
DhruvBO <dhruv.singh@bigohtech.com>
2022-04-05 20:43:00 +0530

    text on last two element ehen user stooped scrolling

--- src/components/ImageIfiniteScroll/index.tsx
+++ src/components/ImageIfiniteScroll/index.tsx
@@ -7,9 +7,14 @@ const ImageIfiniteScroll = () => {
   const [pageNo, setPageNo] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [stoppedScrolling, setStoppedScrolling] = useState(false);
   const [getData, setGetData] = useState<Array<object>>([]);
+  const [idOfLasttwoElement, setIdOfLasttwoElement] = useState({
+    secondLastEle: "",
+    lastEle: "",
+  });
   const LIMIT = 4;
-
+  let isScrolling: any;
   window.onscroll = () => {
     if (
       window.innerHeight + document.documentElement.scrollTop ===
@@ -19,7 +24,6 @@ const ImageIfiniteScroll = () => {
       setPageNo(pageNo + 1);
     }
   };
-
   const getDataRequest = async () => {
     setIsLoading(true);
     try {
@@ -27,6 +31,12 @@ const ImageIfiniteScroll = () => {
         `https://picsum.photos/v2/list?page=${pageNo}&limit=${LIMIT}`
       );
       setGetData([...getData, ...response.data]);
+      setIdOfLasttwoElement({
+        // response.data.length - 1
+        secondLastEle: response.data[response.data.length - 2].id,
+        lastEle: response.data[response.data.length - 1].id,
+      });
+
       setIsLoading(false);
     } catch (error) {
       setErrorMessage("Something went wrong!");
@@ -34,14 +44,25 @@ const ImageIfiniteScroll = () => {
     }
   };
 
+  const checkScrollinStatus = () => {
+    setStoppedScrolling(false);
+    //check if user has stopped scrolling
+    window.clearTimeout(isScrolling);
+    isScrolling = setTimeout(function () {
+      setStoppedScrolling(true);
+      console.log("Scrolling has stopped.");
+    }, 5000);
+  };
+
   useEffect(() => {
+    checkScrollinStatus();
     getDataRequest();
   }, [pageNo]);
 
   return (
     <div>
       <div>{isLoading ? <LoadindSpinner /> : ""}</div>
-      {(errorMessage.length !== 0) ? (
+      {errorMessage.length !== 0 ? (
         <div className="error_msg">
           <h2>{errorMessage}</h2>
         </div>
@@ -52,6 +73,8 @@ const ImageIfiniteScroll = () => {
             key={data.id}
             author={data.author}
             id={data.id}
+            hasStoppedScrolling={stoppedScrolling}
+            lastTwoEle={idOfLasttwoElement}
           />
         ))
       )}
--- src/components/Section/Card/index.tsx
+++ src/components/Section/Card/index.tsx
@@ -6,9 +6,38 @@ type typeProps = {
   downloadUrl: string;
   author: string;
   id: number;
+  hasStoppedScrolling: boolean;
+  lastTwoEle: {
+    secondLastEle: string;
+    lastEle: string;
+  };
 };
-const ImageCard = ({ downloadUrl, author, id }: typeProps) => {
 
+const ImageCard = ({
+  downloadUrl,
+  author,
+  id,
+  hasStoppedScrolling,
+  lastTwoEle,
+}: typeProps) => {
+  // console.log(
+  //   id,
+  //   lastTwoEle.secondLastEle,
+  //   lastTwoEle.lastEle,
+  //   hasStoppedScrolling
+  // );
+  // console.log("outside the condition: ", lastTwoEle, id);
+  const onStopScolling = () => {
+    if (
+      hasStoppedScrolling &&
+      (id.toString() === lastTwoEle.secondLastEle ||
+        id.toString() === lastTwoEle.lastEle)
+    ) {
+      console.log("inside the condition: ", lastTwoEle, id);
+      return true;
+    }
+    return false;
+  };
   return (
     <div className="card_main">
       <div className="card_main__card_main__card_wrapper">
@@ -20,6 +49,11 @@ const ImageCard = ({ downloadUrl, author, id }: typeProps) => {
             <img src={downloadUrl} alt="download_image" />
           </Link>
         </div>
+        {onStopScolling() && (
+          <div>
+            <p>I am the chosen one {id}</p>
+          </div>
+        )}
       </div>
     </div>
   );
